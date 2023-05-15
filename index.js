2c57c82711f379495d3bc568a10f002a61b83166
Lohmarr <119974080+Lohmarr@users.noreply.github.com>
2023-05-14 20:54:30 -0400

    adding new files and imports

--- index.js
+++ index.js
@@ -1,44 +1,23 @@
-const mysql = require("mysql2/promise");
-const dotenv = require("dotenv");
-const mainMenu = require('./scripts/prompts')
+const mainMenu = require("./scripts/prompts");
+const connection = require("./config/connection");
 
-dotenv.config();
+// Import choice arrays
+const {
+  employeeChoices,
+  roleChoices,
+  departmentChoices,
+  managerChoices,
+} = require("./scripts/choices");
 
 async function start() {
-  const connection = await mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-  });
-
-  console.log("Connected to MySQL database.");
-
-  const employeeChoices = [];
-  const roleChoices = [];
-
-  // Put employees in array
-  const employees = await connection.query(
-    'SELECT id, CONCAT(first_name, " ", last_name) AS name FROM employee'
-  );
-  employees[0].forEach((employee) => {
-    employeeChoices.push({
-      value: employee.id,
-      name: employee.name,
-    });
-  });
-
-  // Put roles in array
-  const roles = await connection.query("SELECT id, title FROM role");
-  roles[0].forEach((role) => {
-    roleChoices.push({
-      value: role.id,
-      name: role.title,
-    });
-  });
-
   // Call the mainMenu function to start the application
-  mainMenu(connection, employeeChoices, roleChoices);
+  mainMenu(
+    connection,
+    employeeChoices,
+    roleChoices,
+    departmentChoices,
+    managerChoices
+  );
 }
 
 start();
---
+++ scripts/functions.js
@@ -0,0 +1,19 @@
+viewDepartments() = async () => {
+    const [departments] = await connection.query(`
+       SELECT
+         department.id AS ID, department.name as Name
+       FROM department`);
+        console.table(departments);
+}
+
+viewRoles() = async () => {
+    const [roles] = await connection.query(`
+        SELECT 
+          role.id AS ID, role.title AS Title, 
+          role.salary AS Salary, department.name AS Department 
+        FROM role 
+        INNER JOIN department ON role.department_id = department.id`);
+        console.table(roles);
+}
+
+module.exports = { viewDepartments, viewRoles }
